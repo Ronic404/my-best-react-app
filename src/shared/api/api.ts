219d8f5f95a1c409367fd862072409f3e4fe6463
ROnic404 <ronic@inbox.ru>
2022-11-19 11:18:43 +0400

    fix: Поправлен конфиг api

--- src/shared/api/api.ts
+++ src/shared/api/api.ts
@@ -4,7 +4,12 @@ import { USER_LS_KEY } from 'shared/constants/localStorage'
 
 export const $api = axios.create({
   baseURL: __API__,
-  headers: {
-    authorization: localStorage.getItem(USER_LS_KEY) ?? '',
-  },
+})
+
+$api.interceptors.request.use(config => {
+  if (config.headers) {
+    config.headers.authorization = localStorage.getItem(USER_LS_KEY) ?? ''
+  }
+
+  return config
 })
