73233c4a985006d1bdc49cf6cdc73f4779c5e7cc
ROnic404 <ronic@inbox.ru>
2022-12-04 11:31:35 +0400

    feat: Добавлен генератор создания компонентов

---
+++ scripts/createSlice/templates/createModel.js
@@ -0,0 +1,46 @@
+const fs = require('fs/promises')
+const resolveRoot = require('../resolveRoot')
+const reduxSliceTemplate = require('./reduxSliceTemplate')
+const schemaTypeTemplate = require('./schemaTypeTemplate')
+
+module.exports = async (layer, sliceName) => {
+  const resolveModelPath = (...segments) => resolveRoot('src', layer, sliceName, 'model', ...segments)
+
+  const createModelStructure = async () => {
+    try {
+      await fs.mkdir(resolveModelPath())
+      await fs.mkdir(resolveModelPath('types'))
+      await fs.mkdir(resolveModelPath('slices'))
+      await fs.mkdir(resolveModelPath('selectors'))
+      await fs.mkdir(resolveModelPath('services'))
+    } catch (error) {
+      console.log(`Не удалось создать model сегмент для слайса ${sliceName}`, error)
+    }
+  }
+
+  const createReduxSlice = async () => {
+    try {
+      await fs.writeFile(
+        resolveModelPath('slices', `${sliceName}Slice.ts`),
+        reduxSliceTemplate(sliceName),
+      )
+    } catch (error) {
+      console.log('Не удалось создать редакс слайс', error)
+    }
+  }
+
+  const createSchemaType = async () => {
+    try {
+      await fs.writeFile(
+        resolveModelPath('types', `${sliceName}Schema.ts`),
+        schemaTypeTemplate(sliceName),
+      )
+    } catch (error) {
+      console.log('Не удалось создать тип схемы стейта', error)
+    }
+  }
+
+  await createModelStructure()
+  await createReduxSlice()
+  await createSchemaType()
+}
