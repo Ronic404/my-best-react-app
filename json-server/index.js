6fbb3d9d86b9c88e4b8d6df65fb08c2ab9b20ba5
ROnic404 <ronic@inbox.ru>
2022-12-29 10:23:10 +0400

    feat: Добавлен https-протокол для json-server

--- json-server/index.js
+++ json-server/index.js
@@ -1,6 +1,12 @@
 const fs = require('fs')
 const jsonServer = require('json-server')
 const path = require('path')
+const https = require('https')
+
+const options = {
+  key: fs.readFileSync(path.resolve(__dirname, 'key.pem')),
+  cert: fs.readFileSync(path.resolve(__dirname, 'cert.pem')),
+}
 
 const server = jsonServer.create()
 const router = jsonServer.router(path.resolve(__dirname, 'db.json'))
@@ -42,6 +48,7 @@ server.use((req, res, next) => {
 server.use(router)
 
 // Запуск сервера
-server.listen(8000, () => {
-  console.log('server is running on 8000 port')
+const httpsServer = https.createServer(options.server)
+httpsServer.listen(443, () => {
+  console.log(`server is running on ${443} port`)
 })
